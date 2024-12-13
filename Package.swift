21cadd6b85c4ebf796e587649d4cdce183617545
Yash Shah <67391419+yashshah98@users.noreply.github.com>
2024-12-13 16:45:11 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "RiveRuntime",
-            url: "https://github.com/yashshah98/rive-ios/releases/download/1.0.9/RiveRuntime.xcframework.zip",
-            checksum: "c33a8f690e6f5f4053f320cfc1b2c15f21b28b5a81ed5d6248b4c8d547ce45c3"
+            url: "https://github.com/yashshah98/rive-ios/releases/download/1.0.4/RiveRuntime.xcframework.zip",
+            checksum: "325e622e20bd0a560b1d3ec1032bd1eae69b4e0c30ea81bc0288d51b5bc1e436"
         ),
         .target(
             name: "RiveRuntime-Resources",
